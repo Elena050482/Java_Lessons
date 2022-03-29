e35fd879507f2e1f8a2e9728a1b807ad99ce5035
ElZavarzina <asg707@mail.ru>
2022-03-30 01:08:51 +0300

    исправила замечания

--- HomeWorkOne.java
+++ HomeWorkOne.java
@@ -14,24 +14,24 @@ class HomeWorkOne {
     }
     
     static void checkSumSing() {
-	    int a = 2, b = 3;
-	    System.out.println(a+b>=0? "Сумма положительная" : "Сумма отрицательная");
-	}			
+	int a = 2, b = 3;
+	System.out.println(a+b>=0? "Сумма положительная" : "Сумма отрицательная");
+    }			
 
     static void printColor() {
         int value = 5;
-	    if (value <= 0) {
-	        System.out.println("Красный");
-	    } else if (value <= 100) {
-	        System.out.println("Желтый");
-	    } else {
-	        System.out.println ("Зеленый");
-	    } 
+        if (value <= 0) {
+	    System.out.println("Красный");
+	} else if (value <= 100) {
+	    System.out.println("Желтый");
+	} else {
+	    System.out.println ("Зеленый");
+	} 
     }
 	
     static void compareNumbers() {
         int a = 7;
         int b = 15;
-	    System.out.println(a>=b? "a>=b" : "a<b");
+	System.out.println(a>=b? "a>=b" : "a<b");
     }
 }
