8a81d7396514fb79aa1e56ad2d34f2d8355eba5f
ElZavarzina <asg707@mail.ru>
2022-03-30 01:23:38 +0300

    создала методы по второму заданию

--- HomeWorkTwo.java
+++ HomeWorkTwo.java
@@ -5,35 +5,35 @@
 * @ version 29.03.2022
 */
 class HomeWorkTwo {
-    
+
     public static void main(String[] args) {
-	    System.out.println(checkSum(5, 0));	
-	    System.out.println(checkSum(5, 7));
-	    System.out.println(checkSum(5, 18));
+        System.out.println(checkSum(5, 0));	
+        System.out.println(checkSum(5, 7));
+        System.out.println(checkSum(5, 18));
 	    
-		positiveOrNegative(8);
+        positiveOrNegative(8);
 		
-		System.out.println(isNegative(-8));
-		System.out.println(isNegative(8));
+        System.out.println(isNegative(-8));
+        System.out.println(isNegative(8));
 		
-		printStringTimes("Test", 3);
-	}
+        printStringTimes("Test", 3);
+    }
 		
-	static boolean checkSum(int a, int b) {
-		return (10 <= a + b) && (a + b <= 20);
-	}
+    static boolean checkSum(int a, int b) {
+        return (10 <= a + b) && (a + b <= 20);
+    }
 	
-	static void positiveOrNegative(int a) {
-		System.out.println(a >=0? "Positive" : "Negative");
-	}
+    static void positiveOrNegative(int a) {
+        System.out.println(a >=0? "Positive" : "Negative");
+    }
 	
-	static boolean isNegative(int a) {
-		return a<0;
-	}
+    static boolean isNegative(int a) {
+        return a<0;
+    }
 	
-	static void printStringTimes(String word, int times) {
-		for (int i = 0; i < times; i++) {
-		    System.out.println(word);
+    static void printStringTimes(String word, int times) {
+        for (int i = 0; i < times; i++) {
+            System.out.println(word);
         }
     }
 } 	
