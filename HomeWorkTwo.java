71985c74c894f9479e014de3908947115f9ca1f7
ElZavarzina <asg707@mail.ru>
2022-03-30 00:54:31 +0300

    создала методы по второму уроку

---
+++ HomeWorkTwo.java
@@ -0,0 +1,41 @@
+/**
+* Java 1. Homework 2
+*
+* @ author Elena Zavarzina
+* @ version 30.03.2022
+*/
+class HomeWorkTwo {
+    
+    public static void main(String[] args) {
+	    System.out.println(checkSum(5, 0));	
+	    System.out.println(checkSum(5, 7));
+	    System.out.println(checkSum(5, 18));
+	    
+		positiveOrNegative(8);
+		
+		System.out.println(isNegative(-8));
+		System.out.println(isNegative(8));
+		
+		printStringTimes("Test", 3);
+	}
+		
+	static boolean checkSum(int a, int b) {
+		return (10 <= a + b) && (a + b <= 20);
+	}
+	
+	static void positiveOrNegative(int a) {
+		System.out.println(a >=0? "Positive" : "Negative");
+	}
+	
+	static boolean isNegative(int a) {
+		return a<0;
+	}
+	
+	static void printStringTimes(String word, int times) {
+		for (int i = 0; i < times; i++) {
+		    System.out.println(word);
+        }
+    }
+} 	
+	
+	
